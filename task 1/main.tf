23b3adcfa4af3bf921449156eb501e473342e15d
yuliyakim01 <yulia.kim@du.edu>
2024-09-29 18:09:42 +0500

    added terraform deployment workfow for github actions

--- task 1/main.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket = "terraform-states-bucket-yuliyakim"
-        key = "task1/terraform.tfstate"
-        region = "ap-south-1"
-        dynamodb_table = "terraform-lock-table"
-    }
-}
